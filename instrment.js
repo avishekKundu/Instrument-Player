1587ae46de13448b6a67204255dba3d08f0d3531
Avishek Kundu <119733078+avishekKundu@users.noreply.github.com>
2023-06-05 23:51:26 +0530

    sounds modified

---
+++ instrment.js
@@ -0,0 +1,20 @@
+let accordion = new Audio("Accordion.mp3");
+let drum = new Audio("Drum.mp3");
+let flute = new Audio("Flute.mp3");
+let guitar = new Audio("Guitar.mp3");
+let lute = new Audio("Lute.mp3");
+let lyre = new Audio("Lyre.mp3");
+let mandolin = new Audio("Mandolin.mp3");
+let ocarina = new Audio("Ocarina.mp3");
+let piano = new Audio("Piano.mp3");
+let saxophone = new Audio("Saxophone.mp3");
+let trumpet = new Audio("Trumpet.mp3");
+let violin = new Audio("Violin.mp3");
+
+
+
+
+
+
+
+
