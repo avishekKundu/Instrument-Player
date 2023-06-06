e75fb185758e21500dc14a5dee77ade813a46ed2
Avishek Kundu <119733078+avishekKundu@users.noreply.github.com>
2023-06-06 23:43:01 +0530

    bug fixes

--- instrument.js
+++ instrument.js
@@ -1,21 +1,183 @@
-let accordion = new Audio("Accordion.mp3");
-let drum = new Audio("Drum.mp3");
-let flute = new Audio("Flute.mp3");
-let guitar = new Audio("Guitar.mp3");
-let lute = new Audio("Lute.mp3");
-let lyre = new Audio("Lyre.mp3");
-let mandolin = new Audio("Mandolin.mp3");
-let ocarina = new Audio("Ocarina.mp3");
-let piano = new Audio("Piano.mp3");
-let saxophone = new Audio("Saxophone.mp3");
-let trumpet = new Audio("Trumpet.mp3");
-let violin = new Audio("Violin.mp3");
+let accrdion = new Audio("./MP3_Sounds/Accordion.mp3");
+let drm = new Audio("./MP3_Sounds/Drum.mp3");
+let flte = new Audio("./MP3_Sounds/Flute.mp3");
+let guitr = new Audio("./MP3_Sounds/Guitar.mp3");
+let lte = new Audio("./MP3_Sounds/Lute.mp3");
+let lyr = new Audio("./MP3_Sounds/Lyre.mp3");
+let mandlin = new Audio("./MP3_Sounds/Mandolin.mp3");
+let ocrina = new Audio("./MP3_Sounds/Ocarina.mp3");
+let pian = new Audio("./MP3_Sounds/Piano.mp3");
+let saxphone = new Audio("./MP3_Sounds/Saxophone.mp3");
+let trmpet = new Audio("./MP3_Sounds/Trumpet.mp3");
+let violn = new Audio("./MP3_Sounds/Violin.mp3");
 
-let soundActive = true;
+let soundActive = false;
 
 guitar.onclick = () => {
-    if(soundActive === true) {
-        
+    if (soundActive === false) {
+        guitr.play();
+        guitr.loop = true;
+        guitar.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        guitr.pause();
+        guitar.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+drum.onclick = () => {
+    if (soundActive === false) {
+        drm.play();
+        drm.loop = true;
+        drum.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        drm.pause();
+        drm.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+accordion.onclick = () => {
+    if (soundActive === false) {
+        accrdion.play();
+        accrdion.loop = true;
+        accordion.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        accrdion.pause();
+        accordion.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+saxophone.onclick = () => {
+    if (soundActive === false) {
+        saxphone.play();
+        saxphone.loop = true;
+        saxophone.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        saxphone.pause();
+        saxophone.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+flute.onclick = () => {
+    if (soundActive === false) {
+        flte.play();
+        flte.loop = true;
+        flute.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        flte.pause();
+        flute.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+piano.onclick = () => {
+    if (soundActive === false) {
+        pian.play();
+        pian.loop = true;
+        piano.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        pian.pause();
+        piano.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+trumpet.onclick = () => {
+    if (soundActive === false) {
+        trmpet.play();
+        trmpet.loop = true;
+        trumpet.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        trmpet.pause();
+        trumpet.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+violin.onclick = () => {
+    if (soundActive === false) {
+        violn.play();
+        violn.loop = true;
+        violin.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        violn.pause();
+        violin.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+mandolin.onclick = () => {
+    if (soundActive === false) {
+        mandlin.play();
+        mandlin.loop = true;
+        mandolin.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        mandlin.pause();
+        mandolin.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+lute.onclick = () => {
+    if (soundActive === false) {
+        lte.play();
+        lte.loop = true;
+        lute.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        lte.pause();
+        lute.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+ocarina.onclick = () => {
+    if (soundActive === false) {
+        ocrina.play();
+        ocrina.loop = true;
+        ocarina.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        ocrina.pause();
+        ocarina.textContent = "Play";
+        soundActive = false;
+    }
+}
+
+lyre.onclick = () => {
+    if (soundActive === false) {
+        lyr.play();
+        lyr.loop = true;
+        lyre.textContent = "Stop";
+        soundActive = true;
+    }
+    else {
+        lyr.pause();
+        lyre.textContent = "Play";
+        soundActive = false;
     }
 }
 
